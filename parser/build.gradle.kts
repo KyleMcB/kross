0ee575f9beff9434de8f7c2aad310073f8807b5a
kyle mcburnett <kylemcburnett@xingpeds.com>
2024-11-27 16:34:31 -0800

    test folder in the parser module is up and running

--- parser/build.gradle.kts
+++ parser/build.gradle.kts
@@ -8,4 +8,14 @@ repositories {
 }
 
 dependencies {
+    testImplementation(kotlin("test"))
+
+    // Test engine for your environment
+    testImplementation(kotlin("test-junit")) // For JUnit-based test runner/ Use the Kotlin JUnit 5 integration.
+    // testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    //
+    // // Use the JUnit 5 integration.
+    // testImplementation(libs.junit.jupiter.engine)
+    //
+    // testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ parser/src/test/kotlin/com/xingpeds/kross/parser/ParserTest.kt
@@ -0,0 +1,9 @@
+import kotlin.test.Test
+import kotlin.test.fail
+
+class ParserTest {
+    @Test
+    fun helloWorld() {
+        fail("this test should fail")
+    }
+}
