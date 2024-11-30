e57ac93b276d5b471c6414d1cf757d5060425cd8
kyle mcburnett <kylemcburnett@xingpeds.com>
2024-11-29 18:33:10 -0800

    lexer is now a flow of tokens

---
+++ parser/src/main/kotlin/com/xingpeds/kross/parser/Lexer.kt
@@ -0,0 +1,59 @@
+package com.xingpeds.kross.parser
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+private val whiteSpaceMatcher = Regex("^\\s+")
+
+class Lexer(
+    private val input: String,
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+) {
+    private var cursor = 0
+    fun tokens(): Flow<Token> = flow {
+        while (cursor <= input.lastIndex) {
+            val string = input.substring(cursor)
+            val ws = whiteSpaceMatcher.find(string)
+            if (ws != null) {
+                cursor += ws.value.length
+                continue
+            }
+            val match: Pair<MatchResult, TokenType>? = TokenType.entries.parallelMap(coroutineScope) { tokenType ->
+                val match = tokenType.matcher.find(string)
+                return@parallelMap if (match != null) {
+                    match to tokenType
+                } else {
+                    null
+                }
+            }.filterNotNull().sortedBy { it.second.precedence }.reversed().firstOrNull()
+            if (match != null) {
+                //
+                val token = createTokenFrom(string.substring(0, match.first.value.length), match.second)
+                cursor += match.first.value.length
+                emit(token)
+            } else {
+                throw Exception("Unexpected character at $cursor\n\"$string\"")
+            }
+
+        }
+    }
+
+    private fun createTokenFrom(text: String, tokenType: TokenType): Token {
+        return when (tokenType) {
+            TokenType.Word -> Token.Word(text)
+        }
+    }
+}
+
+suspend fun <A, B> Iterable<A>.parallelMap(
+    scope: CoroutineScope,
+    transform: suspend (A) -> B
+): List<B> = scope.run {
+    map { element ->
+        async { transform(element) }
+    }.awaitAll() // Collect all the deferred results
+}
