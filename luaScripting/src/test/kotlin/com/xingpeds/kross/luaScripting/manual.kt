02eff7b5db624f32b5a208613b1b52ffafffa3e0
kyle mcburnett <kylemcburnett@xingpeds.com>
2024-12-13 19:01:32 -0800

    added new module for lua scripting

--- luaScripting/src/test/kotlin/com/xingpeds/kross/luaScripting/manual.kt
+++ luaScripting/src/test/kotlin/com/xingpeds/kross/luaScripting/manual.kt
@@ -1,10 +1,93 @@
 package com.xingpeds.kross.luaScripting
 
+import org.luaj.vm2.Globals
+import org.luaj.vm2.LoadState
+import org.luaj.vm2.LuaValue
+import org.luaj.vm2.Varargs
+import org.luaj.vm2.compiler.LuaC
+import org.luaj.vm2.lib.*
 import kotlin.test.Test
 
 class manual {
     @Test
     fun hi() {
         println("hi")
+        val globals = Globals().apply {
+            load(BaseLib())
+            load(PackageLib())
+            load(Bit32Lib())
+            load(TableLib())
+            load(StringLib())
+            load(CoroutineLib())
+            LoadState.install(this)
+            LuaC.install(this)
+        }
+
+        fun luaprintln(str: String) {
+            println("LUA_PRINTLN: $str")
+            //kotlin.io.println()
+        }
+
+// Overwrite print function
+        globals["print"] = object : VarArgFunction() {
+            override fun invoke(args: Varargs): Varargs {
+                val tostring = globals["tostring"]
+                val out = (1..args.narg())
+                    .map { tostring.call(args.arg(it)).strvalue()!!.tojstring() }
+                luaprintln(out.joinToString("\t"))
+                return LuaValue.NONE
+            }
+        }
+        val result = globals.load(
+            //language=lua
+            """
+    function max(a, b)
+        if (a > b) then
+            return a
+        else
+            return b
+        end
+    end
+    a = 10
+    res = 1 + 2 + a + max(20, 30)
+    print(res - 1)
+    b = {}
+    b[1] = 10
+    print(b)
+    for i=4,1,-1 do print(i) end
+    
+    
+    co = coroutine.create(function ()
+       for i=1,5 do
+         --print("co", i)
+         coroutine.yield("co" .. i, i + 1)
+       end
+       return "completed"
+     end)
+     
+    function coroutine_it (co)
+      return function ()
+            local code, res = coroutine.resume(co)
+            if code then
+                return res 
+            end
+     end
+    end
+
+    for i in coroutine_it(co) do
+        print("for", i)
+    end
+    
+    --for i=1,12 do
+    --    local code, res = coroutine.resume(co)
+    --    print(code, res)
+    --end
+    print("ENDED!")
+
+    return res
+"""
+        )
+
+        result.call()
     }
 }
