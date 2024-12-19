562534329875a76eee333c880717edc3435fbb88
kyle mcburnett <kylemcburnett@xingpeds.com>
2024-12-19 14:08:53 -0800

    all fixed again. what was I doing?

--- executable/src/test/kotlin/com/xingpeds/kross/executable/JavaOSProcessTest.kt
+++ executable/src/test/kotlin/com/xingpeds/kross/executable/JavaOSProcessTest.kt
@@ -7,48 +7,58 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.seconds
 
 class JavaOSProcessTest {
     private val cwd: File = File(".").absoluteFile
 
     @Test
-    fun one() = runTest {
+    fun one() = runTest(timeout = 5.seconds) {
         val subject = JavaOSProcess()
         val result = subject("echo", listOf("hello"), cwd = cwd, env = emptyMap())()
         assertEquals(0, result)
     }
 
     @Test
-    fun two() = runTest {
+    fun two() = runTest(timeout = 5.seconds) {
         val subject = JavaOSProcess()
         val outputPipe = Pipe()
         val output = StringBuilder()
         CoroutineScope(Dispatchers.Default).launch {
             launch {
+                println("invoke")
                 subject(
                     "echo", listOf("fake input"), cwd = cwd, env = emptyMap(), pipes = Pipes(
                         programOutput = outputPipe
                     )
                 )()
+                outputPipe.close()
+                println("finished invoke")
             }
             launch {
-                outputPipe.inputStream().use { it.copyTo(output.asOutputStream()) }
+                println("connect to output")
+                outputPipe.connectTo(output.asOutputStream())
+
+                println("finished connect to output")
             }
         }.join()
         assertEquals("fake input", output.toString().trim())
     }
 
     @Test
-    fun three() = runTest {
+    fun three() = runTest(timeout = 5.seconds) {
         val subject = JavaOSProcess()
         val result = StringBuilder()
         val outputPipe = Pipe()
         val inputPipe = Pipe()
         val programInput = "fake input"
-        inputPipe.connectTo(programInput.byteInputStream())
         CoroutineScope(Dispatchers.Default).launch {
             launch {
                 outputPipe.connectTo(result.asOutputStream())
+            }
+            launch {
+                inputPipe.connectTo(programInput.byteInputStream())
+                inputPipe.close()
             }
             launch {
                 subject(
@@ -57,6 +67,7 @@ class JavaOSProcessTest {
                         programInput = inputPipe
                     )
                 )()
+                outputPipe.close()
             }
         }.join()
         assertEquals(programInput, result.toString().trim())
